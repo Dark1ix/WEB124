43c88250530299491fbd9a74bee24ac4b80bbac7
Dark1ix <bbb10gamer@gmail.com>
2026-07-27 21:32:11 -0400

    Add files via upload

---
+++ browserDashboard/dashboard.js
@@ -0,0 +1,70 @@
+console.log("Current URL:", location.href);
+console.log("Browser Language:", navigator.language);
+console.log("Online Status:", navigator.onLine);
+
+console.log("Screen Width:", screen.width);
+console.log("Screen Height:", screen.height);
+
+console.log("Window Width:", window.innerWidth);
+console.log("Window Height:", window.innerHeight);
+
+const form = document.forms["userForm"];
+const nameField = form.elements["name"];
+const websiteField = form.elements["website"];
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    let userName = nameField.value;
+    let website = websiteField.value;
+
+    document.getElementById("display").innerHTML =
+        "Name: " + userName + "<br>" +
+        "Favorite Website: " + website;
+
+    try {
+        let websiteURL = new URL(website);
+        console.log("Hostname:", websiteURL.hostname);
+        console.log("Protocol:", websiteURL.protocol);
+        console.log("Pathname:", websiteURL.pathname);
+    } 
+    catch(error) {
+        console.log("Please enter a valid URL with https://");
+    }
+
+    localStorage.setItem("savedName", userName);
+    let storedName = localStorage.getItem("savedName");
+    console.log("Local Storage Name:", storedName);
+
+    sessionStorage.setItem("savedWebsite", website);
+
+    let storedWebsite = sessionStorage.getItem("savedWebsite");
+
+    console.log("Session Storage Website:", storedWebsite);
+});
+
+nameField.addEventListener("focus", function() {
+    console.log("Name field was selected.");
+});
+
+nameField.addEventListener("blur", function() {
+    console.log("Name field was left.");
+});
+
+const backButton = document.getElementById("backButton");
+const forwardButton = document.getElementById("forwardButton");
+
+
+backButton.addEventListener("click", function() {
+    console.log("Back button clicked.");
+    history.back();
+});
+
+
+forwardButton.addEventListener("click", function() {
+    console.log("Forward button clicked.");
+    history.forward();
+});
+
+/*
+ */
